f2156103ae491e629821fad7b219fd9c4c09f16c
loadletter <loadletter@users.noreply.github.com>
2017-05-09 21:45:16 +0200

    Working version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ipfs-userscript
 Userscript to redirect pages from http://gateway.ipfs.io to a local IPFS gateway
 
-Pretty much a copy of this safari extension https://github.com/NeoTeo/ipfs-catcher adapted to userscript
+Based on this safari extension https://github.com/NeoTeo/ipfs-catcher adapted to userscript
 
 A more complete extension for chrome: https://github.com/dylanPowers/ipfs-chrome-extension
--- ipfs-gateway-redirect.user.js
+++ ipfs-gateway-redirect.user.js
@@ -6,11 +6,25 @@
 // @match       *://gateway.ipfs.io/ipns/*
 // @match       *://ipfs.io/ipfs/*
 // @match       *://ipfs.io/ipns/*
-// @version     2
+// @exclude     http://localhost:*/*
+// @version     4
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
 
-var local_gw = "localhost:8080";
+var regExp = /.?(\/ipfs\/|\/ipns\/)?(Qm\w{44})/;
+var matches = regExp.exec(window.location.toString());
 
-location.host = local_gw;
+// If matches is not null it will always have 3 elements, some of 
+// which may be null.
+if (matches != null && matches.length > 1) {
+	// matches[1] is the protocol.
+	var defaultProto = (matches[1] == null) ? "/ipfs/" : matches[1];
+
+	// matches[2] is the hash.
+	var hash = matches[2];
+	if(hash != null) {
+		var newUrl = "http://localhost:8080" + defaultProto + hash;
+		window.location = newUrl;
+	}
+}
